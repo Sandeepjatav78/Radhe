701f345767415381fef227eddb62e4acd94411f9
Sandeep <sy268134@gmail.com>
2026-01-03 17:04:13 +0530

    Video added

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   
   // ✅ CORRECTED LINK (Direct Streaming Link)
   // ID: 1eXG1MFLGhaBhMnf1OeW3MG5BA2QAWL3i
-  const videoUrl = "https://drive.google.com/uc?export=download&id=1eXG1MFLGhaBhMnf1OeW3MG5BA2QAWL3i";
+  const videoUrl = "https://res.cloudinary.com/drld9vkhw/video/upload/v1767439971/RP_ypedig.mp4";
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 font-sans">
